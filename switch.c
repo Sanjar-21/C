6dcb4338fb19e21c8d8263ced369289f8f6743db
Sanjar-21 <oshiqdev1101@gmail.com>
2024-09-22 16:47:51 +0500

    switch case

--- switch.c
+++ switch.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main(void){
-  printf("GGGGG");
+  int num = 1;
+  switch(num){
+    case 1:
+      printf("Num case 1\n");
+      if(num == 1) printf("if ga ham kirdi \n");
+      break;
+    case 2:
+      printf("num teng 3");
+      break;
+    default:
+      printf("hech qaysiga kirmadi mencha");
+      break;
+  }
   return (0);
 }
