5895691a9ba534046d6cb0cf9d212f829e7ba8ea
Sanjar-21 <oshiqdev1101@gmail.com>
2024-09-24 13:47:43 +0500

    len c

--- len.c
+++ len.c
@@ -5,6 +5,11 @@ int main(void){
   char a[] = "sfklsjhfjfvnkjdfvhdfjbnskjdfsfkjsdfvs";
   char g[] = "sanjar";
   int s = 0;
+  int b = 0;
+  for(int i = 0; i < strlen(a); i++){
+    b++;
+  }
+  printf("%d\n", b);
   for(int i = 0; i < strlen(g); i++){
     s++;
   }
