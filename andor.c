8404f9b1daaccfcd51090f7c5900b3b06eac1d37
Sanjar-21 <oshiqdev1101@gmail.com>
2024-09-26 09:53:41 +0500

    andor

--- andor.c
+++ andor.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   // or || and &&
   int y = 2021;
   int g = 2025;
 
-  if (g <= 2025 || g >= 2030) {
+  if (g <= 2025 || g >= 2030)
+  {
     printf("Nice\n");
-  } else
+  }
+  else
     printf("niam\n");
 
-  if (y <= 2021 && y >= 2020) {
+  if (y <= 2021 && y >= 2020)
+  {
     printf("Nice\n");
-  } else
+  }
+  else
     printf("main\n");
   return (0);
 }
