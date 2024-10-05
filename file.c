dd46b937cb3f0f242c14e5bbd1ded9e32efc8410
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-05 07:47:27 +0500

    file ni chalasi bor

--- file.c
+++ file.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void){
-  FILE *file = fopen("io.txt", "a");
-  fputs("Saloam akka afa\n", file);
-
+  // FILE *file = fopen("io.txt", "a");
+  // fputs("Hello World\n", file);
+  // while(true){
+  //   int input = 0;
+  //   printf("Enter # (-1 quit): ");
+  //   scanf("%d\n", &input);
+  //   if(input == -1) break;
+  //   else fprintf(file, "%d\n", input);
+  // }
+  // kiyingi qator;
+  FILE *file = fopen("io.txt", "r");
+  int finput = 0;
+  fscanf(file, "%d\n", &finput);
+  printf("Ninmer: %d\n", finput);
   fclose(file);
 }
