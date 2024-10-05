deed976b5a52fa6d6e898bc3e82b9f41208a5993
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-05 08:00:48 +0500

    arguments

--- arguments.c
+++ arguments.c
@@ -3,5 +3,12 @@
 int main(int argc, char *argv[]){
     printf("argc: %d\n", argc);
 
+    // printf("argv[0] = %s\n", argv[0]);
+    // printf("argv[0] = %s\n", argv[1]);
+    // printf("argv[0] = %s\n", argv[2]);
+    // printf("argv[0] = %s\n", argv[3]);
+    for(int i = 0; i < argc; i++){
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
     return 0;
 }
