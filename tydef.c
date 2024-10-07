babec48249cbab503c968c932ad3721859763457
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-07 12:07:20 +0500

    tydef

---
+++ tydef.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef int kilmetr;
+
+int main(void) {
+  kilmetr termiz, andjon, denov;
+  termiz = 650;
+  andjon = 1200;
+  denov = 700;
+  printf("Termiz : %d\n", termiz);
+  printf("Andjon: %d\n", andjon);
+  printf("Denov: %d", denov);
+  return 0;
+}
