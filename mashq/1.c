40077853a05f322780536fffcb78f3c162b32225
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-13 01:05:08 +0500

    masivni bita son nechta borligini aliqlash

--- mashq/1.c
+++ mashq/1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-
+int main(void) {
+  int arr[] = {
+      1, 24, 5, 6, 7, 8, 92, 3, 2, 2, 3,
+  };
+  int sum = 0;
+  int son = 2;
+  int len = sizeof(arr) / sizeof(arr[0]);
+  for (int i = 0; i < len; i++) {
+    if (arr[i] == son) {
+      sum++;
+    }
+  }
+  printf("sum: %d\n", sum);
 }
