197ae6e75baaa48de1c8b03dc3305505690566e7
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-12 08:21:07 +0500

    malloc

---
+++ malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int *a = malloc(sizeof(int) * 10);
+
+  for (int i = 0; i < 10; i++) {
+    a[i] = 10 - i;
+    printf("a[%d] = %d\n", i, a[i]);
+  }
+  printf("\n\n\n");
+  for (int i = 0; i < 10; i++) {
+    printf("a[%d] = %d\n", i, a[i]);
+  }
+  printf("\n");
+  printf("a: %p\n", a);
+}
