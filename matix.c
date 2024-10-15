027655fdd6b1f0b88b865ed10c7d0bb11c677206
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-15 17:32:27 +0500

    matrix

---
+++ matix.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define ROWS 2
+#define COLS 3
+
+int main(void) {
+  int a[2][3];
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      printf("Enter: a[%d][%d]=", i, j);
+      scanf("%d", &a[i][j]);
+    }
+  }
+}
