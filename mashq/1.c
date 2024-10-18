f55b300f09ed418bd076948abe8874ce92b9c692
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-18 16:57:55 +0500

    masini min topish

--- mashq/1.c
+++ mashq/1.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
+int min(int arr[], int size, int min);
 int main(void) {
-  int n[] = {23, 12, 432, 452, 1, 4};
-  int size = sizeof(n) / sizeof(n[0]);
-  int min = n[0];
+  // int n[] = {23, 12, 432, 452, 1, 4};
+  // int size = sizeof(n) / sizeof(n[0]);
+  // int min = n[0];
+  // for (int i = 1; i < size; i++) {
+  //   if (n[i] < min) {
+  //     min = n[i];
+  //   }
+  // }
+  int array[] = {13, 14, 1, 24, 2, 44};
+  int lenght = sizeof(array) / sizeof(array[0]);
+  int mm = array[0];
+
+  int oo = min(array, lenght, mm);
+  printf("%d\n", oo);
+  return (0);
+}
+int min(int arr[], int size, int min) {
   for (int i = 1; i < size; i++) {
-    if (n[i] < min) {
-      min = n[i];
+    if (arr[i] < min) {
+      min = arr[i];
     }
   }
-  printf("min : %d\n", min);
-  return (0);
+  return min;
 }
