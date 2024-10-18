b1c3d2eb53aeb27c6b6770ef317703c9ed663893
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-18 11:07:21 +0500

    masiv and max

--- mashq/2.c
+++ mashq/2.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  double arr1[] = {21.2, 4.2, 1.1, 3.3, 13.9};
-  double sum = 0;
-  double iii = 0;
-  int len = sizeof(arr1) / sizeof(arr1[0]);
-  for (int i = 0; i < len; i++) {
-    sum += arr1[i];
-    printf("arr1[%d]=%.2lf, sum = %.2lf\n", i, arr1[i], sum);
-  }
+  int n[] = {12, 31, 14, 122, 11};
+  int size = sizeof(n) / sizeof(n[0]);
+  int max = n[0];
 
-  // iii = sum / 2;
-  // printf("iii: %.2lf\n", iii);
+  for (int i = 0; i < size; i++) {
+    if (n[i] > max)
+      max = n[i];
+  }
+  printf("max : %d\n", max);
   return 0;
 }
