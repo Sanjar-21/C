8c19d5b156c0e146aaf3d00f1cb4b481c91b36b4
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-18 21:56:33 +0500

    masivni yarimin almashtirish 1 -> 5 ga

--- mashq/3.c
+++ mashq/3.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-  int a[] = {1, 2, 34, 5, 6, 13, 16, 29, 145, 332, 1};
-  int len = sizeof(a) / sizeof(a[0]);
-  for (int i = 0; i < len; i++) {
-    int jam  = 0;
-    for (int j = i + 1; i < len; i++) {
-      if (a[i] == a[j]) {
-        jam = 1;
-        break;
-      }
-    }
-    if(!jam){
-        printf("a[] { %d }", a[i]);
-    }
+  int arr[] = {12,42,52,53,22,59,11};
+  int lenght = 7;
+  int num;
+
+  for (int i = 1; i < (lenght / 2); i++) {
+    num = arr[i];
+    arr[i] = arr[lenght - i - 1];
+    arr[lenght - i - 1] = num;
   }
-  return 0;
+
+  for (int i = 1; i < lenght; i++) {
+    printf("arr[%d] = %d\n", i, arr[i]);
+  }
 }
