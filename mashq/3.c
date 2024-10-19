297cebddbed578172b29e6774c48f928e757385e
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-19 23:27:41 +0500

    arrayni teskarisga tartiblab chiqarish

--- mashq/3.c
+++ mashq/3.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-  int arr[] = {12,42,52,53,22,59,11};
-  int lenght = 7;
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int lenght = 9;
   int num;
 
-  for (int i = 1; i < (lenght / 2); i++) {
+  for (int i = 0; i < (lenght / 2); i++) {
     num = arr[i];
     arr[i] = arr[lenght - i - 1];
     arr[lenght - i - 1] = num;
   }
 
-  for (int i = 1; i < lenght; i++) {
+  for (int i = 0; i < lenght; i++) {
     printf("arr[%d] = %d\n", i, arr[i]);
   }
 }
