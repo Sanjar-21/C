5ed7219fac77012aa1c3cfe519ae62549f7097b4
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-20 18:01:57 +0500

    dars37 commentlar // /**/

---
+++ dars37.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define LEN 10
+// c da comintlar 2 hil buladi bir qatorni olish va kup qatorli
+int main(void) {
+  // bir ga mislol int arr[] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+
+  /*int num;
+
+  for (int i = 0; i < LEN; i++) {
+    num += arr[i];
+  }
+
+  printf("arrayni urtasi arr = %d\n", num / LEN);
+  return 0;*/
+}
