c82baa023af7008b40053330e00d08c8b7e20c5c
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-21 23:39:06 +0500

    strcat

---
+++ 46.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char set[20] = "This and ";
+    char dest[2] = "or";
+    char *ptr = strcat(set, dest);
+    printf("ptr: %s\n", ptr);
+
+    printf("set: %p\n", set);
+    printf("ptr: %p\n", ptr);
+    // strcat char masivga qushadi
+    return 0;
+}
