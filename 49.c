a003ef225b164577ab7f6fc8b550a2b36922b42d
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-22 23:07:43 +0500

    Ternary Operator

--- 49.c
+++ 49.c
@@ -2,13 +2,18 @@
 #include <string.h>
 
 int main(void) {
-    int a = 4;
-    int b = 9;
+    int a = 9;
+    int b = 8;
     int c = 0;
-    if(a == b) {
-        c = 10;
-    } else {
-        c = 5;
-    }
+    // if(a == b) {
+    //     c = 10;
+    // } else {
+    //     c = 5;
+    // }
+    int num = 0;
+
+    c = (a == b) ? 10 : 5;
+    num = 10 * ((a == c) ? 2 : 1);
     printf("c -->> %d\n", c);
+    printf("num -->>%d\n", num);
 }
