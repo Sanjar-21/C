d198f38b4ca2f2c5c2f38dc0a97714b7bf1e2702
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-23 15:49:39 +0500

    fgets va scanf

--- 47.c
+++ 47.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 int main(void) {
-    char bufor[100];
-    printf(" ");
-    //scanf("%[^\n]%*c", bufor);
-    //printf("bufor: %s\n", bufor);
-    fgets(bufor, 100, stdin);
+  char bufor[100];
+  printf(" ");
+  // scanf("%[^\n]%*c", bufor);
+  // printf("bufor: %s\n", bufor);
+  fgets(bufor, 100, stdin);
 
-    printf("bufor: %s\n", bufor);
-    return 0;
+  printf("bufor: %s\n", bufor);
+  return 0;
 }
