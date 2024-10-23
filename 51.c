202fe1b63da44ac1d6904bd8b0fb4b3f98425402
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-23 16:28:29 +0500

    memset

---
+++ 51.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char buffor[10];
+    // for(int i = 0; i < 10; i++){
+    //     buffor[i] = 'a';
+    // }
+    memset(buffor, 'a', sizeof(char) * 5);
+    memset(buffor+5, 'x', sizeof(char) * 5);
+    for (int i = 0; i < 10; i++) {
+        printf("%c", buffor[i]);
+    }
+}
