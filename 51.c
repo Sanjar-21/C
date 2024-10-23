6763b5ab9110b7dec513cdc4cece8db53de61359
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-23 16:29:07 +0500

    memset in c

--- 51.c
+++ 51.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 int main(void) {
-    char buffor[10];
-    // for(int i = 0; i < 10; i++){
-    //     buffor[i] = 'a';
-    // }
-    memset(buffor, 'a', sizeof(char) * 5);
-    memset(buffor+5, 'x', sizeof(char) * 5);
-    for (int i = 0; i < 10; i++) {
-        printf("%c", buffor[i]);
-    }
+  char buffor[10];
+  // for(int i = 0; i < 10; i++){
+  //     buffor[i] = 'a';
+  // }
+  memset(buffor, 'a', sizeof(char) * 5);
+  memset(buffor + 5, 'x', sizeof(char) * 5);
+  for (int i = 0; i < 10; i++) {
+    printf("%c", buffor[i]);
+  }
+  return 0;
 }
