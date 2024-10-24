aee5f97dcbfa9af48677c30a078081d2dc4fbd57
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-24 09:31:42 +0500

    true false and bool

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    // if (34) printf("true");
+    // else printf("false");
+
+    bool x = true;
+    if (x) printf("true\n");
+    else printf("flase\n");
+
+    bool o = true;
+    bool d = false;
+    printf("%d\n", o);
+    printf("%d\n", d);
+}
