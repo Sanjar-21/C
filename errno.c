3c81c6fe28638203cbdab12e67034cccaadac580
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-24 16:48:44 +0500

    errno in c

---
+++ errno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+int main(void) {
+    FILE *fh = fopen("sds.txt", "r");
+
+    printf("Errno befor: %d\n", errno);
+    if(fh == NULL) {
+        printf("Errno after: %d\n", errno);
+        printf("String strerror: %s\n", strerror(errno));
+    } else {
+        fclose(fh);
+    }
+}
