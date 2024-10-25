1cfad09d856553e97da4d0edcd05aa596425d1b5
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-25 15:10:13 +0500

    pow bu ikki soni darajasiga hisoblash uchun kerak

---
+++ pow.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+  double x = 2.4;
+  double y = 3.1;
+  double resolt = pow(x, y);
+  int a = 3;
+  int b = 3;
+  int num = pow(a, b);
+  printf("num: %d\n", num);
+  printf("resolt: %f\n", resolt);
+  return 0;
+}
