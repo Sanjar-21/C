8f62e22f338cb6fed5dbfb7078c6f7d1387d9f1a
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-26 10:25:05 +0500

    random

--- random.c
+++ random.c
@@ -4,10 +4,14 @@
 
 int main(void) {
   srand(time(NULL));
-  printf("Randim 1: %d\n", rand());
-  printf("Randim 2: %d\n", rand());
-  printf("Randim 3: %d\n", rand());
-  printf("Randim 4: %d\n", rand());
-  printf("Randim 5: %d\n", rand());
+  // printf("Randim 1: %d\n", rand());
+  // printf("Randim 2: %d\n", rand());
+  // printf("Randim 3: %d\n", rand());
+  // printf("Randim 4: %d\n", rand());
+  // printf("Randim 5: %d\n", rand());
+  for (int i = 0; i < 6; i++) {
+    int randomm = 1 + (rand() % 21);
+    printf("%d\n", randomm);
+  }
   return 0;
 }
