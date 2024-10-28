00260563f4d9441a0608b62b08bae350c42ecae1
Sanjar-21 <oshiqdev1101@gmail.com>
2024-10-28 13:57:53 +0500

    system() bu dasturdan tashqari funksiya ls mkdir touch

---
+++ system.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    system("ls");
+    system("echo 'mainsvvsv' ");
+    system("touch main.txt");
+    system("rm main.txt");
+    return 0;
+}
