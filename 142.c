43283cea4f5f41ffb39f13fb1d95ba4e9e98e36f
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-07 22:32:11 +0500

    sqrt sqrtf sqrtl

---
+++ 142.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    double ss = sqrt(9);
+    printf("ss(9) : %f\n", ss);
+
+    double aa = sqrt(9);
+    printf("ss(-9) : %f\n", aa);
+    return 0;
+}
