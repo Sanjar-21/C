33108e610997c89ca5a6a9af8e24f5e0bcc44b68
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-07 22:37:25 +0500

    sqrt sqrtf sqrtl

--- 142.c
+++ 142.c
@@ -7,5 +7,11 @@ int main(void) {
 
     double aa = sqrt(9);
     printf("ss(-9) : %f\n", aa);
+
+    float x = 9.0f;
+    float result = sqrtf(x);
+
+    long double x = 9.0L;
+    long double result = sqrtl(x);
     return 0;
 }
